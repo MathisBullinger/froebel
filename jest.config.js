6e75233b1cf02623b5e3270a2728aaf52a6df636
Mathis Bullinger <mathis@bullinger.dev>
2022-05-26 17:22:44 +0200

    update github action

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  testRegex: '((\\.|/)(test|spec))\\.(js|ts)$',
-  moduleFileExtensions: ['ts', 'js'],
-  modulePathIgnorePatterns: ['<rootDir>/__tests__/utils/*', '<rootDir>/build'],
-  testPathIgnorePatterns: ['<rootDir>/__tests__/build'],
-}
